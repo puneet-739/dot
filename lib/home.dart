b57a87509254084344d8788ae8b29989d9fe97ca
puneet <puneetkashyap20@gmail.com>
2023-11-01 22:49:57 +0530

    added home base class

--- lib/home.dart
+++ lib/home.dart
@@ -1,48 +1,26 @@
 import 'package:flutter/material.dart';
 import 'Paint/painter.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomeBase extends StatefulWidget {
+  const HomeBase({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeBase> createState() => _HomeBaseState();
 }
 
-class _HomePageState extends State<HomePage> {
-  Offset offset = const Offset(-100.0, 0.0);
+class _HomeBaseState extends State<HomeBase> {
   int navigationIndex = 0;
-  @override
-  void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Size screenSize = MediaQuery.of(context).size;
-      double width = screenSize.width / 2.0;
-      double height = screenSize.height / 2.0;
-      offset = Offset(width, height);
-    });
-    super.initState();
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('D\u2B24t '), backgroundColor: Theme.of(context).primaryColorLight),
-      body: [Center(
-          child: GestureDetector(
-            onTap: () => setState(() {}),
-            onPanUpdate: (event) {
-              setState(() {
-                double dx = event.localPosition.dx;
-                double dy = event.localPosition.dy;
-                offset = Offset(dx, dy);
-              });
-            },
-            child: CustomPaint(
-              painter: DotPainter(),
-              foregroundPainter: ForGroundPainter(offset),
-              child: Container(),
-            ),
-          ),
-        ),
-        const SearchMe(), const GameMania(), const ProfileJi()][navigationIndex],
+      body: [
+        const HomePage(),
+        const SearchMe(),
+        const GameMania(),
+        const ProfileJi()
+      ][navigationIndex],
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationIndex,
         onDestinationSelected: (index) {
@@ -71,6 +49,48 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Offset offset = const Offset(-100.0, 0.0);
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Size screenSize = MediaQuery.of(context).size;
+      double width = screenSize.width / 2.0;
+      double height = screenSize.height / 2.0;
+      offset = Offset(width, height);
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () => setState(() {}),
+        onPanUpdate: (event) {
+          setState(() {
+            double dx = event.localPosition.dx;
+            double dy = event.localPosition.dy;
+            offset = Offset(dx, dy);
+          });
+        },
+        child: CustomPaint(
+          painter: DotPainter(),
+          foregroundPainter: ForGroundPainter(offset),
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
+
 
 class SearchMe extends StatelessWidget {
   const SearchMe({Key? key}) : super(key: key);
