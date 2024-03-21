e571523df8f13eedd528b97cf3c6c02002dbb1e6
puneet <puneetkashyap20@gmail.com>
2024-03-22 00:05:35 +0530

    set device orientations

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:dot/view/profilePage/bloc/theme_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 import 'view/homeBase/presentation/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
